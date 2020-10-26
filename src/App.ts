c70ab51964f1c6d2bf0810348e4d3ce555c3defb
Guilherme Caldeira Godoy da Silva <guilhermecaldeira.godoy@gmail.com>
2020-10-25 22:35:01 -0300

    feat(dijkstra): add dijkstra implementation

--- src/App.ts
+++ src/App.ts
@@ -1,12 +1,13 @@
 /*
   
 */
+import { dir } from 'console';
 import { readFileSync } from 'fs';
 import { resolve } from 'path';
 
 type adjacent = {
   cost: number;
-  node: Node;
+  nodeReference: string;
 };
 
 class Node {
@@ -19,20 +20,22 @@ class Node {
   constructor(name: string, first: boolean = false) {
     this.name = name;
     this.closed = false;
+    this.adjacents = [];
     this.dv = first ? 0 : Number.MAX_SAFE_INTEGER;
   }
 
-  public registerAdjacent(node: Node, cost: number) {
-    const find = this.adjacents.find((el) => el.node.name === node.name);
+  public registerAdjacent(name: string, cost: number) {
+    const find = this.adjacents.find((el) => el.nodeReference === name);
     if (find) return;
     this.adjacents.push({
       cost,
-      node,
+      nodeReference: name,
     });
   }
 }
 
 class App {
+  private fileData: string;
   private params: string[];
   private directed: number;
   private graph: Node[];
@@ -40,42 +43,107 @@ class App {
 
   constructor() {
     // Carrega o arquivo em uma string
-    const fileData = readFileSync(resolve(__dirname, 'grafo.txt'), { encoding: 'utf8' });
+    try {
+      this.fileData = readFileSync(resolve(__dirname, 'grafo.txt'), { encoding: 'utf8' });
+    } catch {
+      console.log('Carga do arquivo falhou! Verifique o nome.');
+      return;
+    }
     // Carrega os parãmetros em um array
-    this.params = fileData.split('\n');
+    this.params = this.fileData.split('\n');
     // Carrega a variável de informação de direcionamento
-    this.directed = parseInt(this.params[0]);
-    // this.init(this.params);
+    try {
+      this.directed = this.handleDirectedParam();
+    } catch {
+      console.log('Carga do arquivo falhou! Parâmetro gráfico direcionado, linha 2, inválido.');
+      return;
+    }
+    this.graph = [];
+    this.openNodes = [];
+    try {
+      this.init(this.params);
+    } catch {
+      console.log('Carga do arquivo falhou! Verifique os parâmetros de vertices e arestas.');
+    }
+  }
+
+  handleDirectedParam() {
+    const directed = parseInt(this.params[0]);
+    if (directed === 1 || directed === 0) {
+      return directed;
+    }
+    throw new Error('Unhandled directed param');
   }
 
   private registerNode(node: Node) {
     const find: Node = this.graph.find((el) => el.name === node.name);
     if (find) return;
     this.graph.push(node);
-    this.openNodes.push(node);
   }
 
-  public dijkstra(initialNode: Node) {
-    console.log('Parâmetros: ', this.params);
-    console.log('Direcionado? ', this.directed === 1 ? 'sim' : 'não');
-    if (!this.openNodes) return this.graph;
-    const sortedNodes: Node[] = this.openNodes.sort((a: Node, b: Node) =>
-      a.dv < b.dv ? -1 : a.dv > b.dv ? 1 : 0,
-    );
-    const nextNode = sortedNodes[0];
-    this.openNodes = this.openNodes.filter((el) => el.name !== nextNode.name);
-    nextNode.adjacents.forEach((el) => {
-      if (el.node.closed) return;
+  private handleNodeNames(params: string[]): void {
+    const nodeNames = params.slice(2, parseInt(params[1]) + 2);
+    console.log('Vertices: ', nodeNames);
+    nodeNames.forEach((nodeName) => {
+      if (nodeNames[0] === nodeName) this.registerNode(new Node(nodeName, true));
+      this.registerNode(new Node(nodeName));
+    });
+  }
 
-      this.graph.forEach((node: Node) => {
-        if (node.name === el.node.name) {
-          const relax = el.node.dv + el.cost;
-          node.dv = relax > node.dv ? node.dv : relax;
-          node.predecessor = el.node;
+  private handleArrests(params: string[]): void {
+    const arrests = params.slice(parseInt(params[1]) + 2, params.length);
+    console.log('Arestas: ', arrests);
+    arrests.forEach((el) => {
+      const params = el.split(',');
+      this.graph.forEach((node) => {
+        if (node.name === params[0]) {
+          node.registerAdjacent(params[1], parseInt(params[2]));
         }
       });
     });
   }
+
+  private init(params: string[]) {
+    this.handleNodeNames(params);
+    this.handleArrests(params);
+  }
+
+  public dijkstra(): void {
+    console.log(this.directed === 1 ? 'Direcionado' : 'Não direcionado');
+    try {
+      this.openNodes = this.graph;
+
+      while (this.openNodes.length > 0) {
+        const sortedNodes: Node[] = this.openNodes.sort((a: Node, b: Node) =>
+          a.dv < b.dv ? -1 : a.dv > b.dv ? 1 : 0,
+        );
+
+        const nextOpenNode = sortedNodes[0];
+
+        this.openNodes = this.openNodes.filter((el) => el.name !== nextOpenNode.name);
+
+        nextOpenNode.adjacents.forEach((adjacent) => {
+          const node = this.graph.find((node) => node.name === adjacent.nodeReference);
+
+          if (node.closed) return;
+
+          this.graph.forEach((u: Node) => {
+            if (u.name === adjacent.nodeReference) {
+              const relax = nextOpenNode.dv + adjacent.cost;
+              u.dv = relax > u.dv ? u.dv : relax;
+              node.predecessor = u;
+            }
+          });
+        });
+      }
+    } catch {
+      console.log('O grafo fornecido está incorreto! Verifique a formatação do arquivo.');
+    }
+
+    // return this.graph;
+  }
 }
 
 const app = new App();
+
+app.dijkstra();
